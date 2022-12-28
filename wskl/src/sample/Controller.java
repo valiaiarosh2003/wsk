5c594dd1ff2cf8c19a20bfb0215350f438c37f23
Валентина <77850073+valiaiarosh2003@users.noreply.github.com>
2022-12-28 14:17:03 +0300

    Add files via upload

---
+++ wskl/src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    public Button btn;
+    @FXML
+    public TextField login;
+    @FXML
+    public PasswordField pass;
+    @FXML
+    public void initialize(){
+        
+    }
+}
